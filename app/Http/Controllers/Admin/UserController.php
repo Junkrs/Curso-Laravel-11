7fb5b5e1d32bb3b8a49d29f74e48641f4d8b5891
Gabriel Junqueira <junqueirabr158@gmail.com>
2024-06-25 10:21:39 -0300

    Finished class 08

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -10,9 +10,23 @@ class UserController extends Controller
 {
     public function index() 
     {
-        $user = User::first();
-        return view('admin.user.index', [
-            'user' => $user
+        $users = User::paginate(15);//User::all();
+        //dd($users); //trecho de codigo usado para debugar variaveis
+
+        return view('admin.users.index', [
+            'users' => $users
         ]);
+    }
+
+    public function create() 
+    {
+        return view('admin.users.create');
+    }
+
+    public function store(Request $request) 
+    {
+        User::create($request->all()); 
+
+        return redirect()->route('users.index');
     }
 }
