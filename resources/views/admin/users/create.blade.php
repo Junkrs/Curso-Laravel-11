dc637ae5607aac857ba1b883cf7280a588797681
Gabriel Junqueira <junqueirabr158@gmail.com>
2024-06-25 10:49:02 -0300

    Finished class 09

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,9 +1,15 @@
-<h1>Novo usuário</h1>
+@extends('admin.layouts.app')
+@section('title', 'Adicionar usuário')
 
-<form action="{{ route('users.store') }}" method="POST">
-    @csrf()
-    <input type="text" name="name" placeholder="Nome">
-    <input type="email" name="email" placeholder="E-mail">
-    <input type="password" name="password" placeholder="Senha">
-    <button type="submit">Enviar</button>
-</form>
+@section('content')
+    <h1>Novo usuário</h1>
+
+    <form action="{{ route('users.store') }}" method="POST">
+        @csrf()
+        <input type="text" name="name" placeholder="Nome">
+        <input type="email" name="email" placeholder="E-mail">
+        <input type="password" name="password" placeholder="Senha">
+        <button type="submit">Enviar</button>
+    </form>
+
+@endsection
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('admin.layouts.app')
+@section('title', 'Lista de usuários')
+
+@section('content')
     <h1>Usuários</h1>
 
     <a href="{{ route('users.create') }}">Novo</a>
@@ -34,5 +30,5 @@
     </table>
 
     {{ $users -> links() }}
-</body>
-</html>
+
+@endsection
