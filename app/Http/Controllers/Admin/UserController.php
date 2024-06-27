1d8f7b791043b3a1ed8f99acde9dbdab998b8e12
Gabriel Junqueira <junqueirabr158@gmail.com>
2024-06-27 09:00:27 -0300

    Finished class 19

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\UpdateUserRequest;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class UserController extends Controller
 {
@@ -72,6 +73,9 @@ class UserController extends Controller
 
     public function destroy(string $id)
     {
+       /* if (Gate::denies('is-admin')) {
+            return back()->with('message', 'Você não pode deletar um usuário sem ser admin');
+        }*/
         if (!$user = User::find($id)) {
             return redirect()->route('users.index')->with('message', 'Usuário não encontrado');
         }
